9ecde3c168af4d01f61e3ebd900e2f69bdb43a46
derrickcreamer <derrickcreamer@gmail.com>
2020-11-28 20:27:43 -0500

    Add shift-dir to continue walking in chosen direction

--- ForaysIntoNorrendrin/UI/ScreenUI/GameRunUI/GameEventHandler/ChooseActionHandler.cs
+++ ForaysIntoNorrendrin/UI/ScreenUI/GameRunUI/GameEventHandler/ChooseActionHandler.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Threading;
 using Forays;
 using GameComponents;
 using GameComponents.DirectionUtility;
 using static ForaysUI.ScreenUI.StaticInput;
+using static ForaysUI.ScreenUI.StaticScreen;
 
 namespace ForaysUI.ScreenUI{
 	// This class uses 'partial' so these large methods can be in separate files at no runtime cost.
@@ -14,114 +16,91 @@ namespace ForaysUI.ScreenUI{
 	// GameEventHandler.cs (contains the constructors and helpers)
 	// StatusEventHandler.cs
 	public partial class GameEventHandler : GameUIObject{
+		private Dir8? walkDir;
+
 		private void ChooseAction(PlayerTurnEvent e){
+			if(walkDir != null){
+				if(Input.KeyIsAvailable){
+					Input.FlushInput();
+					walkDir = null;
+				}
+				else{
+					Point targetPoint = Player.Position.PointInDir(walkDir.Value);
+					if(TileTypeAt(targetPoint) == TileType.Wall || CreatureAt(targetPoint) != null){ //todo
+						walkDir = null;
+					}
+					else{
+						if(!Screen.WindowUpdate()) Program.Quit();
+						Thread.Sleep(10); //todo, make configurable
+						e.ChosenAction = new WalkAction(Player, Player.Position.PointInDir(walkDir.Value));
+						return;
+					}
+				}
+			}
 			ConsoleKeyInfo key = Input.ReadKey();
+			bool shift = (key.Modifiers & ConsoleModifiers.Shift) == ConsoleModifiers.Shift;
 			switch(key.Key){
 				case ConsoleKey.UpArrow:
 				case ConsoleKey.NumPad8:
-					ChooseActionFromDirection(e, Dir8.N);
+					ChooseActionFromDirection(e, Dir8.N, shift);
 					break;
 				case ConsoleKey.RightArrow:
 				case ConsoleKey.NumPad6:
-					ChooseActionFromDirection(e, Dir8.E);
+					ChooseActionFromDirection(e, Dir8.E, shift);
 					break;
 				case ConsoleKey.LeftArrow:
 				case ConsoleKey.NumPad4:
-					ChooseActionFromDirection(e, Dir8.W);
+					ChooseActionFromDirection(e, Dir8.W, shift);
 					break;
 				case ConsoleKey.DownArrow:
 				case ConsoleKey.NumPad2:
-					ChooseActionFromDirection(e, Dir8.S);
+					ChooseActionFromDirection(e, Dir8.S, shift);
 					break;
 				case ConsoleKey.NumPad9:
-					ChooseActionFromDirection(e, Dir8.NE);
+					ChooseActionFromDirection(e, Dir8.NE, shift);
 					break;
 				case ConsoleKey.NumPad3:
-					ChooseActionFromDirection(e, Dir8.SE);
+					ChooseActionFromDirection(e, Dir8.SE, shift);
 					break;
 				case ConsoleKey.NumPad1:
-					ChooseActionFromDirection(e, Dir8.SW);
+					ChooseActionFromDirection(e, Dir8.SW, shift);
 					break;
 				case ConsoleKey.NumPad7:
-					ChooseActionFromDirection(e, Dir8.NW);
+					ChooseActionFromDirection(e, Dir8.NW, shift);
 					break;
 			}
-			// todo, old code pasted:
-			/*
-					while(true) {
-						if(walkDir != null) {
-							if(w.KeyPressed) {
-								w.GetKey();
-								walkDir = null;
-							}
-							else {
-								n.Event.ChosenAction = new WalkAction(g.Player, g.Player.Position.Value.PointInDir(walkDir.Value));
-								Thread.Sleep(30);
-								return;
-							}
-						}
-						if(w.KeyPressed) {
-							Dir8? dir = null;
-							switch(w.GetKey()) {
-								case Key.N:
-									n.Event.ChosenAction = new DescendAction(g.Player);
-									return;
-									//n.Event.ChosenAction = new FireballEvent(g.Player, null);
-								case Key.Escape:
-									g.Suspend = true;
-									return;
-							}
-							if(dir != null) {
-								Point targetPoint = g.Player.Position.Value.PointInDir(dir.Value);
-								if(g.Creatures[targetPoint] == null) {
-									if(g.Player.TileTypeAt(targetPoint) == TileType.Wall) { // Check for wall sliding:
-										Point cwPoint = g.Player.Position.Value.PointInDir(dir.Value.Rotate(true));
-										Point ccwPoint = g.Player.Position.Value.PointInDir(dir.Value.Rotate(false));
-										if(g.Player.TileTypeAt(cwPoint) != TileType.Wall && g.Player.TileTypeAt(ccwPoint) == TileType.Wall) {
-											dir = dir.Value.Rotate(true);
-										}
-										else if(g.Player.TileTypeAt(cwPoint) == TileType.Wall && g.Player.TileTypeAt(ccwPoint) != TileType.Wall) {
-											dir = dir.Value.Rotate(false);
-										}
-									}
-									n.Event.ChosenAction = new WalkAction(g.Player, g.Player.Position.Value.PointInDir(dir.Value));
-									if(w.KeyIsDown(Key.ShiftLeft) || w.KeyIsDown(Key.ShiftRight)) walkDir = dir;
-								}
-								else {
-									n.Event.ChosenAction = new AttackAction(g.Player, g.Creatures[targetPoint]);
-									lastMsg = "You strike!";
-								}
-								return;
-							}
-						}
-						if(!w.WindowUpdate()) {
-							g.Suspend = true;
-							g.GameOver = true;
-							return;
-						}
-						else Thread.Sleep(10);
-			*/
-			//
 		}
-		private void ChooseActionFromDirection(PlayerTurnEvent e, Dir8 dir){
+		private void ChooseActionFromDirection(PlayerTurnEvent e, Dir8 dir, bool shift){
 			Point targetPoint = Player.Position.PointInDir(dir);
+			Creature targetCreature = CreatureAt(targetPoint);
+			if(targetCreature != null){
+				e.ChosenAction = new MeleeAttackAction(Player, targetCreature);
+				return;
+			}
 			// Check for wall sliding:
-			if(CreatureAt(targetPoint) == null && TileTypeAt(targetPoint) == TileType.Wall){ //todo
+			bool wallSliding = false;
+			if(TileTypeAt(targetPoint) == TileType.Wall){ //todo, terrain types etc.
 				Point cwPoint = Player.Position.PointInDir(dir.Rotate(true));
 				Point ccwPoint = Player.Position.PointInDir(dir.Rotate(false));
 				if(TileTypeAt(cwPoint) != TileType.Wall && TileTypeAt(ccwPoint) == TileType.Wall) {
-					targetPoint = Player.Position.PointInDir(dir.Rotate(true));
+					wallSliding = true;
+					dir = dir.Rotate(true);
+					targetPoint = Player.Position.PointInDir(dir);
+					targetCreature = CreatureAt(targetPoint);
 				}
 				else if(TileTypeAt(cwPoint) == TileType.Wall && TileTypeAt(ccwPoint) != TileType.Wall) {
-					targetPoint = Player.Position.PointInDir(dir.Rotate(false));
+					wallSliding = true;
+					dir = dir.Rotate(false);
+					targetPoint = Player.Position.PointInDir(dir);
+					targetCreature = CreatureAt(targetPoint);
 				}
 			}
-			Creature targetCreature = CreatureAt(targetPoint);
-			if(CreatureAt(targetPoint) != null){
+			if(targetCreature != null){
 				e.ChosenAction = new MeleeAttackAction(Player, targetCreature);
 			}
 			else{
 				e.ChosenAction = new WalkAction(Player, targetPoint);
+				if(shift && !wallSliding) walkDir = dir; // Don't set walkDir for attacks or wall slides
 			}
 		}
 	}
